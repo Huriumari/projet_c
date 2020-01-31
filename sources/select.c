946117385e46abd169fa67ede5072d1f951d7213
huriumari <clementbossard@free.fr>
2020-01-31 01:35:26 -0800

    Add select feature to project

--- sources/select.c
+++ sources/select.c
@@ -2,10 +2,11 @@
 
 char	is_above_component(component_t *component, double mouse_x, double mouse_y){
 	GdkPixbuf 	*pb;
+	double		x,y;
 
 	pb = gtk_image_get_pixbuf(GTK_IMAGE(component->img));
-	x = gdk_pixbuf_get_width(pb);
-		y = gdk_pixbuf_get_height(pb);
+	x = (double)gdk_pixbuf_get_width(pb);
+	y = (double)gdk_pixbuf_get_height(pb);
 		if (mouse_x > component->pos.x - x/2
 		&&	mouse_x < component->pos.x + x/2
 		&&	mouse_y > component->pos.y - y/2
@@ -20,6 +21,11 @@ void	select_component(data_t *data, double x, double y){
 
 	component = data->component;
 	while (component != NULL){
-		
+		if (is_above_component(component, x, y))
+			component->is_select = 1;
+			else
+				if (component->is_select)
+					component->is_select = 0;
+	component = component->next;
 	}
 }
