8064e710a03ef7b0072eb3b554efd039784fa95a
huriumari <clementbossard@free.fr>
2020-02-01 09:00:41 -0800

    Most of the load function is done

--- sources/save_functions.c
+++ sources/save_functions.c
@@ -46,3 +46,39 @@ int		save(data_t *data, char *path){
 	free(path);
 	return 1;
 }
+
+int		load(data_t *data, char *path){
+	FILE		*file;
+	char		buffer[255];
+	component_t	*component, start = NULL;
+	size_t		max_id = 0;
+	int			i;
+
+	file = fopen(path, "r");
+	if (file == NULL)
+		return 0;
+
+	while(!feof(file)){
+		component = malloc(sizeof(component));
+		if (component == NULL)
+			return 0;
+		fscanf(file, "%lu:%s:%lf:%lf:%d",
+				component->id,
+				component->name,
+				component->pos.x,
+				component->pos.y,
+				(int)component->number_parts);
+		for (i = 0 ; i < (int)component->number_parts ; i++)
+			fscanf(file,":%lf:%lf:%c",
+				component->parts[i].pos.x,
+				component->parts[i].pos.y,
+				component->parts[i].type);
+		fgets(buffer, 255, file);
+		if (component->id > max_id)
+			max_id = component->id;
+		//creer composant
+		component->next = start;
+		start = component;
+	}
+	//effacer ancienns listes
+}
